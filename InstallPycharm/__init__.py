b554b30a5a6241f0af27f4da2e21f7d21bd1cb32
sunwang33 <330463670@qq.com>
2018-11-01 22:19:27 +0800

    初始化提交。

---
+++ InstallPycharm/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+#-*- coding：utf-8 -*-
+# @Time    ：2018/11/1 20:56
+# @Author  ：SunWang
+# @File    ：__init__.py.py
---
+++ InstallPycharm/demon1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding：utf-8 -*-
+# @Time    ：2018/11/1 20:59
+# @Author  ：SunWang
+# @File    ：demon1.py
+import sys
+
+# name = input("Please input your name: ")
+# print("hello {0}".format(name))
+for i in range(1,10):
+    print(i)
