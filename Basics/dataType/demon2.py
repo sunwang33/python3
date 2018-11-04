4002b7a0cc1deedcb14397a3852d16ea6555fa29
sunwang33 <330463670@qq.com>
2018-11-04 14:01:10 +0800

    2.1 运算符

--- Basics/dataType/demon2.py
+++ Basics/dataType/demon2.py
@@ -14,15 +14,49 @@
 #<=0   False
 
 #浮点型   float
-a = 3.141592653
-m = round(a , 2)
-print(m)
+# a = 3.141592653
+# m = round(a , 2)
+# print(m)
 
 #字符串  str 'abc' "abc"  '''abc'''
-string = 'abcdefghigh'
-print(string)
+# string = 'abacdaefaghaiagah'
+# print(string)
 
 #find
+# result = string.find('def')
+# print(result)
+# print(string[:])
+
+#replace  替换
+# print(string.replace('a','AAA'))
+
+#split  分隔符
+# print(string.split('a'))
+#结果：['', 'b', 'cd', 'ef', 'gh', 'i', 'g', 'h']
+
+#strip  去除字符串左右空格
+string = '  abacdaefaghaiagah  '
+print(string.strip())
+
+#字符串打印
+print ("my string is : %s " %string )
+string1 = 123
+string2 = 456
+print ("my string is {0}, {1} ,{2} ".format(string,string1,string2)) #推荐
+
+#字符串拼接
+print("hel"+"lo")
+#结果:hello
+
+#join(可迭代的对象)  #一般是一个list
+newList = string.strip().split('a')
+print(" ### ".join(newList))
+#结果：### b ### cd ### ef ### gh ### i ### g ### h
+
+
+
+
+
 
 
 
